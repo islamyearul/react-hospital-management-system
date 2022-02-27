f8941e85677902afe26eaddc7ffaa2d84a5adbaf
islamyearul <yearulislamonem@gmail.com>
2022-02-27 18:59:41 +0600

    Templating about and Blog 2

--- src/includes/Nav.js
+++ src/includes/Nav.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
 
 class Nav extends Component {
+
+ 
     render() {
+
         return (
             <>
                 <div className="header-topbar style-2">
